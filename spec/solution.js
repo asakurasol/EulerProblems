e285c184659d86819d288b1a9ace0e5b6c50e03b
asakurasol <mamamia1734@gmail.com>
2015-02-19 19:41:35 -0800

    finished up to problem 4

---
+++ spec/solution.js
@@ -0,0 +1,40 @@
+/* global solution, describe, it, expect, should */
+
+describe('multiplesThreeFive', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(multiplesThreeFive).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
+
+describe('largest prime factor', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(multiplesThreeFive).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
+
